e07eb5ead505c3966dda0af3091acb0eeff2a6cd
Sebastian Bernal <sbernal2018@gmail.com>
2018-02-01 01:04:35 -0500

    centering and scripts implemented

--- Nuappsapp/SwipingGestures.js
+++ Nuappsapp/SwipingGestures.js
@@ -1,3 +1,6 @@
+
+
+/*
 var swipe = document.getElementById('swipe');
 
 var cool = new Hammer(swipe);
@@ -7,3 +10,7 @@ cool.get('swipe').set({direction:Hammer.DIRECTION_ALL});
 cool.on("swipeup",
     function(menu){
     swipe.textContent = menu.type+"gesture detected";});
+
+*/
+var menu = document.getElementbyId('hide_this');
+menu.style.display = "none";
